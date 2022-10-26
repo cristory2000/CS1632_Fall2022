aadf4c6ee4b51f76f98f279d29ade459e934557a
Cristopher  Story <cristory2000@gmail.com>
2022-10-26 19:41:16 -0400

    Update datarace.c

--- exercises/Supplement3/datarace.c
+++ exercises/Supplement3/datarace.c
@@ -2,22 +2,28 @@
 #include <pthread.h>
 
 int shared = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *add(void *unused) {
+pthread_mutex_lock(&mutex);
   for(int i=0; i < 1000000; i++) { 
     shared++;
   }
+pthread_mutex_unlock(&mutex);
   return NULL;
 }
 
 int main() {
   pthread_t t;
+
   // Child thread starts running add
   pthread_create(&t, NULL, add, NULL);
   // Main thread starts running add
+
   add(NULL);
   // Wait until child thread t terminates
   pthread_join(t, NULL);
   printf("shared=%d\n", shared);
   return 0;
 }
+
